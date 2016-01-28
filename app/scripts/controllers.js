a8589d304fe5910e091b91f0c3152eec59455da1
Maria Katsourani <contact@mariakatsourani.com>
2016-01-28 20:53:25 +0100

    w03l04

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -35,9 +35,9 @@ angular.module('confusionApp')
 
     }])
 
-    .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-
-        var dish = menuFactory.getDish(3);
+  .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+    
+        var dish= menuFactory.getDish(parseInt($stateParams.id,10));
 
         $scope.dish = dish;
 
@@ -100,7 +100,7 @@ angular.module('confusionApp')
             date    : ""
           };
         
-        }
+        };
 
       }])
 
